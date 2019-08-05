00a91b0d242332bd5f151f8354ae9fd3b6076f5c
mrjadaml <mrjadaml@gmail.com>
2019-08-04 17:22:24 -0700

    first test ✅

--- components/atoms/button/test.js
+++ components/atoms/button/test.js
@@ -1,7 +1,11 @@
+import React from 'react';
 import Button from './index';
+import { render } from '@testing-library/react';
+import { getByText } from '@testing-library/dom';
 
 describe('Button', () => {
-  it('should do stuff', () => {
-    console.log('yeaaaa')
+  it('should render children', () => {
+    const { getByText } = render(<Button>Hi Bob</Button>);
+    getByText('Hi Bob');
   });
 });
