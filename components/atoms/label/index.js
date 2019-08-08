9eb7eb603ede158418e7f5e7ececdb9a821f8104
mrjadaml <mrjadaml@gmail.com>
2019-08-08 07:56:48 -0700

    label atom

---
+++ components/atoms/label/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './styles.css';
+
+const Label = ({ children, htmlFor, ...rest }) => {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={styles.label}
+      {...rest}
+    >
+      {children}
+    </label>
+  )
+};
+
+Label.propTypes = {
+  children: PropTypes.node.isRequired,
+  htmlFor: PropTypes.string.isRequired,
+};
+
+Label.defaultProps = {
+};
+
+export default Label;
